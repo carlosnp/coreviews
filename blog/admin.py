2275b883b819f2b278ad7f9cebedf57992db7a21
carlosnp <machinery.e@gmail.com>
2019-04-28 07:20:51 -0400

    modelo inicial del blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import PostModel
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ('active','title','publish')
+    list_display_links  = ['title']
+    list_editable       = ['active','publish']
+    class Meta:
+        model = PostModel
+
+admin.site.register(PostModel, PostModelAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
+# Django
 from django.db import models
+from django.utils.encoding import smart_text
 
-# Create your models here.
+# Opciones del campo publish
+PUBLISH_CHOICES	= [
+		('draft', 'Draft'),
+		('publish', 'Publish'),
+		('private', 'Private'),
+	]
+
+#Modelo del Post
+class PostModel(models.Model):
+	active 	= models.BooleanField(default=True)
+	title 	= models.CharField(max_length=100)
+	content = models.TextField(null=True, blank=True)
+	# Campo con opciones
+	publish = models.CharField(max_length=120, choices=PUBLISH_CHOICES, default='draft')
+
+	class Meta:
+		verbose_name='Post'
+		verbose_name_plural = 'Posts'
+
+	def __str__(self):
+		return smart_text(self.title)
+
+	def __unicode__(self):
+		return smart_text(self.title)

ff91f8a203d1ce11f35880ca1a0ec463d5f70a2b
machinery <machinery.e@gmail.com>
2019-04-29 16:59:42 -0400

    get

--- blog/views.py
+++ blog/views.py
@@ -12,13 +12,19 @@ class Blog_Detail_View(DetailView):
     template_name = "blog/detail.html"
 
     def get_context_data(self, **kwargs):
-        context = super(Blog_Detail_View,self).get_context_data(**kwargs)
-        return context
+        qs = super(Blog_Detail_View,self).get_context_data(**kwargs)
+        qs = qs.order_by("-timestamp")
+        return qs
     
 # List
 class Blog_List_View(ListView):
     model = PostModel
     template_name = "blog/list.html"
+
+    def get_queryset(self, *args,**kwargs):
+        return super(Blog_List_View, self).get_queryset(*args,**kwargs)
+
+    
 
     def get_context_data(self, *args,**kwargs):
         context = super(Blog_List_View,self).get_context_data(*args,**kwargs)

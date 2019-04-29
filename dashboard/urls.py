93705f70339a3c80130bccadfeb782476102ab19
machinery <machinery.e@gmail.com>
2019-04-29 13:02:57 -0400

    home page

---
+++ dashboard/urls.py
@@ -0,0 +1,15 @@
+# Django
+from django.urls import path, include
+# Project
+from .views import HomeView
+
+app_name = "dashboards"
+
+urlpatterns = [
+    path("", HomeView.as_view(), name="home")
+    # path('', post_list_view, name="list"),
+    # path('create/', post_create_view, name="create"),
+    # path('<int:id>/', post_detail_view, name="detail"),
+    # path('<int:id>/update', post_update_view, name="update"),
+    # path('<int:id>/delete', posts_delete_view, name="delete"),
+]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,14 @@
+# Django
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+# Home Page
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+    # Context Data
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args,**kwargs)
+        context["title"] = "Home Page" 
+        return context
+    

61018d62e53e7184f2ced0001f80b89e05334fe8
machinery <machinery.e@gmail.com>
2019-04-29 09:54:56 -0400

    admin change

--- blog/admin.py
+++ blog/admin.py
@@ -4,9 +4,13 @@ from django.contrib import admin
 from .models import PostModel
 
 class PostModelAdmin(admin.ModelAdmin):
-    list_display = ('active','title','publish','publish_date','updated','timestamp')
+    list_display = ['active','title',
+                    'publish','publish_date',
+                    'updated','timestamp',
+                    ]
     list_display_links  = ['title']
     list_editable       = ['active','publish','publish_date']
+    readonly_fields = ['updated','timestamp']
     class Meta:
         model = PostModel
 

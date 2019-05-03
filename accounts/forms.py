c6dd4e4179b53ebe790f30d1a756988ab8a2c5eb
machinery <machinery.e@gmail.com>
2019-05-03 14:54:17 -0400

    inicio

---
+++ accounts/forms.py
@@ -0,0 +1,5 @@
+# Django
+from django import forms
+# Django Translation
+from django.utils.translation import gettext as _
+from django.utils.translation import ugettext_lazy as _l
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+# python
+from datetime import timedelta, datetime, date
+# Django
 from django.db import models
-
-# Create your models here.
+from django.utils import timezone
+from django.utils.text import slugify
+from django.utils.encoding import smart_text
+from django.utils.timesince import timesince
+from django.db.models.signals import pre_save, post_save
+# Django Translation
+from django.utils.translation import gettext as _
+from django.utils.translation import ugettext_lazy as _l

7471b635155d8174fabf29b20d3ed59929ce2246
machinery <machinery.e@gmail.com>
2019-05-03 15:55:15 -0400

    primer modelo

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
+# Django
 from django.contrib import admin
+# Project
+from .models import Profile
 
-# Register your models here.
+class AccountsAdmin(admin.ModelAdmin):
+     list_display = ['user', 'city']
+
+     class Meta:
+         model = Profile
+    
+# Admin Register
+admin.site.register(Profile, AccountsAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@
 from datetime import timedelta, datetime, date
 # Django
 from django.db import models
+from django.conf import settings
 from django.utils import timezone
 from django.utils.text import slugify
 from django.utils.encoding import smart_text
@@ -10,3 +11,36 @@ from django.db.models.signals import pre_save, post_save
 # Django Translation
 from django.utils.translation import gettext as _
 from django.utils.translation import ugettext_lazy as _l
+
+# Perfil de Usuario
+class Profile(models.Model):
+    user = models.OneToOneField(
+                    settings.AUTH_USER_MODEL, 
+                    verbose_name=_l("User"), 
+                    on_delete=models.CASCADE)
+    city = models.CharField(
+                    max_length=150,
+                    verbose_name=_l("City"), 
+                    null=True, 
+                    blank=True)
+
+    class Meta:
+        verbose_name = _l("User")
+        verbose_name_plural = _l("Users")
+
+    def __str__(self):
+        return self.user.username
+    
+    def __unicode__(self):
+        return self.user.username
+    
+    # def get_absolute_url(self):
+    #     return reverse("_detail", kwargs={"pk": self.pk})
+
+# post_save
+def accounts_user_model_post_save_receiver(sender, instance, created, *args, **kwargs):
+    try:
+        Profile.objects.create(user=instance)
+    except:
+        pass
+post_save.connect(accounts_user_model_post_save_receiver, sender = settings.AUTH_USER_MODEL)
